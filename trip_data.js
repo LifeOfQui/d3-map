d3ea37488796d91deefc99a6f112f75c823bba6e
Quirin Koch <quirin.koch@web-dienstleister.de>
2017-12-21 15:13:16 +0100

    Initial commit for d3 Map

---
+++ trip_data.js
@@ -0,0 +1,118 @@
+var home = [{
+    "lon": 11.425754,
+    "lat": 48.766535,
+    "name": "Ingolstadt, Germany",
+    "country": "276",
+    "posts": {
+        "NoStory": ""
+    }
+}];
+
+var countries_holiday = [
+    {
+        "id": "276",
+        "date": "",
+        "year": 2016
+    },
+    {
+        "id": "020",
+        "date": "",
+        "year": 2016
+    },
+    {
+        "id": "300",
+        "date": "",
+        "year": 2016
+    },
+    {
+        "id": "040",
+        "date": "",
+        "year": 2016
+    },
+    {
+        "id": "360",
+        "date": "",
+        "year": 2016
+    },
+    {
+        "id": "144",
+        "date": "",
+        "year": 2016
+    },
+    {
+        "id": "724",
+        "date": "",
+        "year": 2016
+    },
+    {
+        "id": "100",
+        "date": "",
+        "year": 2016
+    },
+    {
+        "id": "156",
+        "date": "",
+        "year": 2016
+    },
+    {
+        "id": "764",
+        "date": "",
+        "year": 2016
+    },
+    {
+        "id": "826",
+        "date": "",
+        "year": 2016
+    },
+    {
+        "id": "380",
+        "date": "",
+        "year": 2016
+    },
+    {
+        "id": "250",
+        "date": "",
+        "year": 2016
+    },
+    {
+        "id": "203",
+        "date": "",
+        "year": 2016
+    }
+];
+
+var countries_ocr = ["276", "020", "300", "040"];
+
+var trip_data = [
+    {
+        "lon": 106.845599,
+        "lat": -6.2087634,
+        "name": "Jarkata, Java, Indonesia",
+        "country": "360",
+        "links": {
+            "NoLink": ""
+        }
+    },
+    {
+        "lon": 16.373819,
+        "lat": 48.208174,
+        "name": "Vienna, Austria",
+        "country": "040",
+        "links": {
+            "NoLink": ""
+        }
+    },
+    {
+        "lon": 27.713918,
+        "lat": 42.688330,
+        "name": "Sunny Beach, Bulgaria",
+        "country": "100",
+        "links": {
+            "NoLink": ""
+        }
+    }
+];
+
+var icon_links = {
+    "NoLink": ""
+};
