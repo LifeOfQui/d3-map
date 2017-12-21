d1c5391e24c3cc20f9b67c8e3d3d825f0ae859d6
Quirin Koch <quirin.koch@web-dienstleister.de>
2017-12-21 15:49:44 +0100

    Added zoom functionality

---
+++ travel_points_java.js
@@ -0,0 +1,57 @@
+var travel_points_java = [
+    {
+		"name" : "München",
+        "lon" : 11.581980,
+		"lat" : 48.135125
+	},
+    {
+		"name" : "Jakarta",
+        "lon" : 106.845599,
+		"lat" : -6.2087634
+	},
+    {
+		"name" : "Pramuka Island",
+        "lon" : 106.613658,
+		"lat" : -5.745962
+	},
+    {
+		"name" : "Jakarta",
+        "lon" : 106.845599,
+		"lat" : -6.2087634
+	},
+    {
+		"name" : "Cianjur",
+        "lon" : 107.142544,
+		"lat" : -6.816824
+	},
+    {
+		"name" : "Bandung",
+        "lon" : 107.619123,
+		"lat" : -6.917464
+	},
+    {
+		"name" : "Yogyakarta",
+        "lon" : 110.369490,
+		"lat" : -7.795580
+	},
+    {
+		"name" : "Karimunjava",
+        "lon" : 110.432639,
+		"lat" : -5.875963
+	},
+    {
+		"name" : "Semarang",
+        "lon" : 110.438125,
+		"lat" : -7.005145
+	},
+    {
+		"name" : "Jakarta",
+        "lon" : 106.845599,
+		"lat" : -6.2087634
+	},
+    {
+		"name" : "München",
+        "lon" : 11.581980,
+		"lat" : 48.135125
+	}
+];
